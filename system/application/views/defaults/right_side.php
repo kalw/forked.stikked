4513d892f86c9ed021f3d3aa70ab524514012d93
Regis Despres <rdespres@Kandor.local>
2011-05-09 11:30:03 +0200

    pastepin alike design files added

---
+++ system/application/views/defaults/right_side.php
@@ -0,0 +1,19 @@
+<div id="content_right">
+										<div class="content_right_menu">
+						<div class="content_right_title"><span title="Change this menu" onclick="location.href='/settings'" class="min_max_span max_it"></span><a href="/login">My Pastes</a></div>
+											</div>
+					<div class="content_right_menu">
+						<div class="content_right_title"><span title="Change this menu" onclick="location.href='/settings'" class="min_max_span min_it"></span><a href="/archive">Public Pastes</a></div>
+						<ul class="right_menu"><li><a href="/KQnziiVM">Untitled</a><span>C | 10 sec ago</span></li><li><a href="/5T7JM5i8">Slow Motion Glitch</a><span>10 sec ago</span></li><li><a href="/UeMek7ni">birthday</a><span>13 sec ago</span></li><li><a href="/NGAcHKE6">Untitled</a><span>28 sec ago</span></li><li><a href="/5bADK7t0">Untitled</a><span>48 sec ago</span></li><li><a href="/HcBy137n">Untitled</a><span>53 sec ago</span></li><li><a href="/ZtPyawpb">Untitled</a><span>51 sec ago</span></li><li><a href="/pSWaiRqt">local jsc  x d+ hd</a><span>51 sec ago</span></li></ul>		
+					</div>
+					
+		<div class="content_right_menu">
+			<div class="content_right_title"><span title="Set Layout Width To: Full Screen" onclick="location.href='/layout.php'" class="min_max_span narrow_it"></span><a href="/layout.php">Layout Width</a></div>	
+		</div>					
+		<div class="content_right_menu">
+			<div class="content_right_title"><a href="">Awesome Users</a></div>
+						
+		<div class="menu_users">
+		</div>		
+					</div>			
+				</div>
