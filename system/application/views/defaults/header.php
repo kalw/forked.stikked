7add2b4e50966cfa9ced4e8972bc50091546a44c
Regis Despres <rdespres@Kandor.local>
2011-05-10 09:01:21 +0200

    header modified to feet signup and login pages

--- system/application/views/defaults/header.php
+++ system/application/views/defaults/header.php
@@ -19,11 +19,11 @@
 			<div id="header_top">
 				<span class="span_left more">Forked.Stikked</span><span class="span_left less"> &nbsp;|&nbsp; paste tool </span>
 				<ul class="top_menu">
-					<li class="no_border_li"><a href="<?php echo config_item('base_url');  ?>/">create new paste</a></li>
-					<li><a href="<?php echo config_item('base_url');  ?>/tools">tools</a></li>
-					<li><a href="<?php echo config_item('base_url');  ?>/api">api</a></li>
-					<li><a href="<?php echo config_item('base_url');  ?>/?lists">archive</a></li>
-					<li><a href="<?php echo config_item('base_url');  ?>/faq">faq</a></li>
+					<li class="no_border_li"><a href="<?php echo config_item('base_url');  ?>">create new paste</a></li>
+					<li><a href="<?php echo config_item('base_url');  ?>tools">tools</a></li>
+					<li><a href="<?php echo config_item('base_url');  ?>api">api</a></li>
+					<li><a href="<?php echo config_item('base_url');  ?>lists">archive</a></li>
+					<li><a href="<?php echo config_item('base_url');  ?>faq">faq</a></li>
 					<li class="twitter"><a href="http://twitter.com/pastebincom">twitter</a></li>
 
 				</ul>
@@ -45,7 +45,7 @@
 					</ul>
 				</div>
 				<ul class="top_menu">
-					<li class="no_border_li"><a href="/signup">sign up</a></li><li><a href="/login">login</a></li><li><a href="/settings">my settings</a></li><li><a href="/profile">my profile</a></li>				
+					<li class="no_border_li"><a href="<?php echo config_item('base_url');  ?>/auth/register">sign up</a></li><li><a href="<?php echo config_item('base_url');  ?>/auth/login">login</a></li><li><a href="<?php echo config_item('base_url');  ?>settings">my settings</a></li><li><a href="<?php echo config_item('base_url');  ?>profile">my profile</a></li>				
 				</ul>		
 			</div>			
 		</div>
