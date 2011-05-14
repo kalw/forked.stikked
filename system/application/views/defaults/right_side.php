41a257616a2328d7265f60b3e4b9afef58900af4
Regis Despres <rdespres@Kandor.local>
2011-05-14 22:59:03 +0200

    Dynamic public pastes added

--- system/application/views/defaults/right_side.php
+++ system/application/views/defaults/right_side.php
@@ -1,19 +1,43 @@
-<div id="content_right">
-										<div class="content_right_menu">
-						<div class="content_right_title"><span title="Change this menu" onclick="location.href='/settings'" class="min_max_span max_it"></span><a href="/login">My Pastes</a></div>
-											</div>
-					<div class="content_right_menu">
-						<div class="content_right_title"><span title="Change this menu" onclick="location.href='/settings'" class="min_max_span min_it"></span><a href="/archive">Public Pastes</a></div>
-						<ul class="right_menu"><li><a href="/KQnziiVM">Untitled</a><span>C | 10 sec ago</span></li><li><a href="/5T7JM5i8">Slow Motion Glitch</a><span>10 sec ago</span></li><li><a href="/UeMek7ni">birthday</a><span>13 sec ago</span></li><li><a href="/NGAcHKE6">Untitled</a><span>28 sec ago</span></li><li><a href="/5bADK7t0">Untitled</a><span>48 sec ago</span></li><li><a href="/HcBy137n">Untitled</a><span>53 sec ago</span></li><li><a href="/ZtPyawpb">Untitled</a><span>51 sec ago</span></li><li><a href="/pSWaiRqt">local jsc  x d+ hd</a><span>51 sec ago</span></li></ul>		
-					</div>
-					
-		<div class="content_right_menu">
-			<div class="content_right_title"><span title="Set Layout Width To: Full Screen" onclick="location.href='/layout.php'" class="min_max_span narrow_it"></span><a href="/layout.php">Layout Width</a></div>	
-		</div>					
-		<div class="content_right_menu">
-			<div class="content_right_title"><a href="">Awesome Users</a></div>
-						
-		<div class="menu_users">
-		</div>		
-					</div>			
-				</div>
+  <div id="content_right">
+<?php if($this->tank_auth->is_logged_in()){?>
+    <div class="content_right_menu">
+      <div class="content_right_title">
+        <a href="/login">My Pastes</a>
+      </div>
+    </div>
+<?php }?>
+
+    <div class="content_right_menu">
+      <div class="content_right_title">
+        <a href="/archive">Public Pastes</a>
+      </div>
+	<?php 
+	$data = $this->pastes->getLists();
+	$count=0;
+	foreach($data['pastes'] as $paste) {
+	$count++;
+	if($count == 5){break;}
+	?>
+      <ul class="right_menu">
+        <li><a href="<?php echo site_url("view/".$paste['pid']) ?>"><?php echo $paste['title']  ?></a><span><?php echo $paste['name']  ?> | <?php  $p = explode(",", timespan($paste['created'], time())); echo $p[0];  ?> ago</span></li>
+      </ul>
+	<?php }?>
+
+    </div>
+
+    <div class="content_right_menu">
+      <div class="content_right_title">
+        <a href="/layout.php">Layout Width</a>
+      </div>
+    </div>
+
+    <div class="content_right_menu">
+      <div class="content_right_title">
+        <a href="">Awesome Users</a>
+      </div>
+
+      <div class="menu_users"></div>
+    </div>
+  </div>
+
+
